e4760dd73ba8621d812a66e17af0480511ddde47
welcomehyunseo <92869725+welcomehyunseo@users.noreply.github.com>
2021-12-02 07:01:12 -0800

    save for atomic design

---
+++ src/components/pages/Webbox.tsx
@@ -0,0 +1,30 @@
+function Header() {
+  return <></>;
+}
+
+function Left() {
+  return <></>;
+}
+
+function Center() {
+  return <></>;
+}
+
+function Right() {
+  return <></>;
+}
+
+function Webbox() {
+  return (
+    <>
+      <Header></Header>
+      <>
+        <Left></Left>
+        <Center></Center>
+        <Right></Right>
+      </>
+    </>
+  );
+}
+
+export default Webbox;
